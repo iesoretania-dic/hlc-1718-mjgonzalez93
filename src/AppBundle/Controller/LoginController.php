1950f46687c8afaccb0325dfb68a093cf60ce72b
mjgonzalez93 <m.jesus_1993@hotmail.com>
2018-02-21 14:03:27 +0100

    Implementado logeo de usuarios para la aplicacion y la herencia de roles

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Usuario;
+use AppBundle\Form\Type\VehiculoType;
+use AppBundle\Repository\AlquilerRepository;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/entrar", name="usuario_entrar")
+     */
+    public function entrarAction(AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+        $ultimoUsuario = $authUtils->getLastUsername();
+
+        return $this->render('seguridad/entrar.html.twig', [
+            'ultimo_usuario' => $ultimoUsuario,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/salir", name="usuario_salir")
+     */
+    public function salirAction()
+    {
+
+    }
+
+}
--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -3,13 +3,15 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UsuarioRepository")
  * @ORM\Table(name="usuario")
  */
-class Usuario
+class Usuario implements UserInterface
 {
     /**
      * @ORM\Id
@@ -394,5 +396,77 @@ class Usuario
     public function getCliente()
     {
         return $this->cliente;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        $roles = ['ROLE_USER'];
+
+        if ($this->getAdministrador()) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        if ($this->getMecanico()) {
+            $roles[] = 'ROLE_MECANICO';
+        }
+
+        if ($this->getComercial()) {
+            $roles[] = 'ROLE_COMERCIAL';
+        }
+
+        if ($this->getCliente()) {
+            $roles[] = 'ROLE_CLIENTE';
+        }
+
+        return $roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->getDni();
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
